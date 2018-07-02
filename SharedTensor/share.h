90082932d0d33a062f8011809ce2f1acb14c8c80
Lianke Qin <1026193951@sjtu.edu.cn>
2018-07-02 17:46:33 +0800

    Update share.h

--- SharedTensor/share.h
+++ SharedTensor/share.h
@@ -7,17 +7,15 @@ using namespace std;
 
 template<class T>
 class Share {
-
-private:
-	int nDev=4;
-	bool flag[8];
-	T mean ;
-	bool meanReady = false;
 public:
+    bool flag[8]; //default max num of GPUs on single machine is 8 here
+    T mean ;
+    bool meanReady = false;
+    int nDev=8; //you need set it on your own, I do not write a constructor for initialization of nDev
     T data[8];
     Share(){
         memset(flag,false,sizeof(bool));
-        
+        meanReady = false;
     }
 	bool Push(T input,int index) {
 
@@ -32,28 +30,36 @@ public:
 
 	bool MeanReady() {
 		if (!meanReady) {
-            if(!flag[0]) return false;
-
-			for (int i = 0; i < nDev; i++) {
-				if (!flag[i]) return false;
-				else data[0] += data[i];
+           	     if(!flag[0]){ 
+			std::cout<<"not ready 0"<<std::endl;
+			return false;
 			}
-			mean = data[0] * 1.0f /  nDev;
-			meanReady = true;
-		}
-		return true;
+		     for (int i = 1; i < nDev; i++) {
+			if (!flag[i]){
+			    std::cout<<"not ready "<<i<<std::endl;
+			    return false;
+			}
+			else data[0] += data[i];
+		     }
+		     mean = data[0]; //remember division by nDev is done before data is pushed into sharedTensor, because I met some error when division is done in meanReady()
+	           
+		     meanReady = true;
+		     return true;
+		}else 
+		     return true;
 	}
 
 	void ResetMean() {
 		for (int i = 0; i < nDev; i++) {
 			if (flag[i]) return;
 		}
+		//std::cout<<"reset mean!"<<std::endl;
 		meanReady = false;
 	}
 
 	T Pop(int index) {
 		while(!MeanReady());//deadlock may occur 
-       		flag[index] = false;
+        	flag[index] = false;
 		T tmp = mean;
 		ResetMean();
 		return tmp;    
